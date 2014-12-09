f0fe09ceed33fbed9ec01ffad0fc8d5731ab2cf4
numtel <bgreen787@gmail.com>
2014-12-08 20:32:18 -0800

    0.0.12: Fixed bug with new trigger implementation

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'numtel:mysql',
   summary: 'MySQL support with Reactive Select Subscriptions',
-  version: '0.0.11',
+  version: '0.0.12',
   git: 'https://github.com/numtel/meteor-mysql.git'
 });
 
