cd949a4ee551b5ce3b0affb7ef5f40c0057c0bb4
Ben Green <ben@latenightsketches.com>
2015-02-14 13:37:34 -0800

    mysql-live-select updated to 0.0.14

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: 'numtel:mysql',
   summary: 'MySQL support with Reactive Select Subscriptions',
-  version: '0.1.0',
+  version: '0.1.1',
   git: 'https://github.com/numtel/meteor-mysql.git'
 });
 
 Npm.depends({
   'mysql': '2.5.4',
-  'mysql-live-select': '0.0.13'
+  'mysql-live-select': '0.0.14'
 });
 
 Package.onUse(function(api) {
