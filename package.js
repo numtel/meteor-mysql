44929ab7a0bc195cda314dd1e45b84460bcaf691
Ben Green <ben@latenightsketches.com>
2015-06-05 22:50:44 +0000

    Patch release 0.1.9

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'numtel:mysql',
   summary: 'MySQL support with Reactive Select Subscriptions',
-  version: '0.1.8',
+  version: '0.1.9',
   git: 'https://github.com/numtel/meteor-mysql.git'
 });
 
